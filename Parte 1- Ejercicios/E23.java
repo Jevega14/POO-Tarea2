1c48ddc9868889246b3ae43df802ac198be3bd73
Jessica Vega <111544730+Jevega14@users.noreply.github.com>
2022-09-02 00:00:05 -0500

    Add files via upload

--- Parte 1- Ejercicios/E23.java
+++ Parte 1- Ejercicios/E23.java
@@ -4,25 +4,25 @@ public class E23 {
     
     public static void main(String[] args){
         
-        Scanner scanner = new Scanner (System.in);
-        
-        int A,B,C;
-        double X1,X2;
+        try (Scanner scanner = new Scanner (System.in)) {
+            int A,B,C;
+            float X1,X2;
 
-        System.out.println("Introduce el coeficiente de segundo grado:");
-        A= scanner.nextInt();
+            System.out.println("Introduce el coeficiente de segundo grado:");
+            A= scanner.nextInt();
 
-        System.out.println("Introduce el coeficiente de primer grado:");
-        B= scanner.nextInt();
+            System.out.println("Introduce el coeficiente de primer grado:");
+            B= scanner.nextInt();
 
-        System.out.println("Introduce el termino independiente:");
-        C= scanner.nextInt();
+            System.out.println("Introduce el termino independiente:");
+            C= scanner.nextInt();
 
-        X1 = (double)((-(B)+(Math.sqrt((B*B)-4*A*C)))/(2*A));
-        X2 = (double)((-(B)-(Math.sqrt((B*B)-4*A*C)))/(2*A));
+            X1 = (float)((-(B)+(Math.sqrt((B*B)-4*A*C)))/(2*A));
+            X2 = (float)((-(B)-(Math.sqrt((B*B)-4*A*C)))/(2*A));
 
 
-        System.out.println("El resultado de X1 es: "+X1);
-        System.out.println("El resultado de X2 es: "+X2);
+            System.out.println("El resultado de X1 es: "+X1);
+            System.out.println("El resultado de X2 es: "+ X2 );
+        }
     }
 }
